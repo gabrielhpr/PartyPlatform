9243ca0da92fa9bc3112dc3df311d276a78af182
Gabriel Rodrigues <gabrielhprdeveloper@gmail.com>
2022-05-24 01:10:38 -0300

    Fixed issues to launch backend.

--- backend/db/conn.ts
+++ backend/db/conn.ts
@@ -6,7 +6,7 @@ const conn = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    port: process.env.MYSQL_PORT,
+    port: Number(process.env.MYSQL_PORT),
     database: process.env.MYSQL_DATABASE
 });
 
